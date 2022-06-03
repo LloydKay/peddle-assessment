5dbc9ec25408c1ff8313851033a0adf3b87a9e5f
Kehinde Ajibola <t@LordWesley-Macbook-Pro.local>
2022-06-03 04:40:56 +0100

    designed repos template

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,66 +2,52 @@ import { useEffect } from "react";
 
 import mrt from "../components/assets/mrt.png";
 
-function User({ repos }) {
-  const {
-    name,
-    id,
-    description,
-    html_url,
-    forks,
-    open_issues,
-    Watchers_count,
-    stargazers_count,
-  } = repos;
-
+function User() {
   return (
     <>
-      {repos > 0 &&
-        repos.map((_repo) => (
-          <div className="w-full mx-auto lg:w-10/12 m-10 p-10" key={id}>
-            <div
-              className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 "
-              // key={index}
-            >
-              <div className="custom-card-image mb-6 md:mb-0">
-                <img src="" alt="" />
-              </div>
+      <div className="w-full mx-auto lg:w-10/12 m-10 p-10">
+        <div
+          className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 "
+          // key={index}
+        >
+          <div className="custom-card-image mb-6 md:mb-0">
+            <img src={mrt} alt="" />
+          </div>
 
-              <div className="col-span-2">
-                <div className="mb-6">
-                  <h1 className="text-3xl card-title">{name}</h1>
-                  <p>Lorem ipsum dolor sit. Lorem, ipsum dolor.</p>
-                </div>
+          <div className="col-span-2 mx-12">
+            <div className="mb-6 mx-7">
+              <h1 className="text-3xl card-title">lo</h1>
+              <p>Lorem ipsum dolor sit. Lorem, ipsum dolor.</p>
+            </div>
 
-                <div>
-                  <span className="inline-flex items-center m-2 px-3 py-1 bg-green-200 hover:bg-green-300 rounded-full text-sm font-semibold text-green-600">
-                    <svg
-                      className="w-4 h-4 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M0 0h24v24H0V0z" fill="none" />
-                      <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z" />
-                    </svg>
-                    <span className="ml-1">Success Badge</span>
-                  </span>
+            <div className="mx-7">
+              <span className="inline-flex items-center m-2 px-3 py-1 bg-green-200 hover:bg-green-300 rounded-full text-sm font-semibold text-green-600">
+                <svg
+                  className="w-4 h-4 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z" />
+                </svg>
+                <span className="ml-1">Success Badge</span>
+              </span>
 
-                  <span className="inline-flex items-center m-2 px-3 py-1 bg-green-200 hover:bg-green-300 rounded-full text-sm font-semibold text-green-600">
-                    <svg
-                      className="w-4 h-4 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M0 0h24v24H0V0z" fill="none" />
-                      <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z" />
-                    </svg>
-                    <span className="ml-1">Success Badge</span>
-                  </span>
-                </div>
-              </div>
+              <span className="inline-flex items-center m-2 px-3 py-1 bg-green-200 hover:bg-green-300 rounded-full text-sm font-semibold text-green-600">
+                <svg
+                  className="w-4 h-4 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z" />
+                </svg>
+                <span className="ml-1">Success Badge</span>
+              </span>
             </div>
           </div>
-        ))}
+        </div>
+      </div>
     </>
   );
 }
